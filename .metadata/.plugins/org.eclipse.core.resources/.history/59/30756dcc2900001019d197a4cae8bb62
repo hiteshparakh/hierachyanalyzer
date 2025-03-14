5890629c1e90135bd8e2b671c5ba39806204aecf
unknown <hiteshparakh@gmail.com>
2025-03-14 16:40:16 +0530

    Initial commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/59/30756dcc2900001019d197a4cae8bb62
@@ -0,0 +1,22 @@
+package com.abc.model;
+
+public class EmployeeTree<Employee> {
+	
+	private Node<Employee> rootEmployee;
+
+	public Node<Employee> getRootEmployee() {
+		return rootEmployee;
+	}
+
+	public void setRootEmployee(Node<Employee> rootEmployee) {
+		this.rootEmployee = rootEmployee;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeeTree [rootEmployee=" + rootEmployee + "]";
+	}
+	
+	
+
+}
---
+++ EmployeeHierarchyAnalyzer/src/main/java/com/abc/model/Employee.java
@@ -0,0 +1,52 @@
+package com.abc.model;
+
+import java.util.Objects;
+
+public class Employee {
+	
+	private int employeeId;
+	private String employeeFirstName;
+	private String employeeLastName;
+	private int employeeSalary;
+	
+	public Employee(int employeeId, String employeeFirstName, String employeeLastName, int employeeSalary) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeFirstName = employeeFirstName;
+		this.employeeLastName = employeeLastName;
+		this.employeeSalary = employeeSalary;
+	}
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeFirstName() {
+		return employeeFirstName;
+	}
+	public void setEmployeeFirstName(String employeeFirstName) {
+		this.employeeFirstName = employeeFirstName;
+	}
+	public String getEmployeeLastName() {
+		return employeeLastName;
+	}
+	public void setEmployeeLastName(String employeeLastName) {
+		this.employeeLastName = employeeLastName;
+	}
+	public int getEmployeeSalary() {
+		return employeeSalary;
+	}
+	public void setEmployeeSalary(int employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", employeeFirstName=" + employeeFirstName + ", employeeLastName="
+				+ employeeLastName + "]";
+	}
+
+	
+}
